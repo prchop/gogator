bacd3136d50fc5dd9778cbfb2ea6356edc2081c0
prchop <182120329+prchop@users.noreply.github.com>
2025-10-23 12:09:39 +0700

    feat: implement register command and update login logic
    - Added new handlerRegister func to create user
    - Update login flow to check if user already exist

--- gogator.go
+++ gogator.go
@@ -1,16 +1,22 @@
 package gogator
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 	"github.com/prchop/gogator/internal/config"
+	"github.com/prchop/gogator/internal/database"
 )
 
 type state struct {
 	cfg *config.Settings
+	db  *database.Queries
 }
 
 type command struct {
@@ -43,11 +49,52 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	name := cmd.args[0]
-	if err := s.config.SetUser(name); err != nil {
-		return fmt.Errorf("couldn't set current user: %v", err)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user %q: %w", name, err)
 	}
-	fmt.Printf("user has been set to %q\n", name)
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("User %q switched successfully!\n", name)
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		log.Printf("Usage: %s <username>\n", cmd.name)
+		return fmt.Errorf("username is required")
+	}
+
+	name := cmd.args[0]
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Name:      name,
+		})
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("User %q created successfully:\n", name)
+	printUser(user)
+	return nil
+}
+
+func printUser(u database.User) {
+	fmt.Printf(" * ID:      %v\n", u.ID)
+	fmt.Printf(" * Name:    %v\n", u.Name)
 }
 
 func Run() {
@@ -56,9 +103,18 @@ func Run() {
 		log.Fatalf("error reading config: %v\n", err)
 	}
 
-	programState := &state{config: &cfg}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v\n", err)
+	}
+	defer db.Close()
+	dbq := database.New(db)
+
+	programState := &state{cfg: &cfg, db: dbq}
 	cmds := &commands{registeredCmds: make(map[string]handler)}
-	cmds.register("login", loginHandler)
+
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
