a0653250e178df1ea12d9116058b425c00ca6a42
prchop <182120329+prchop@users.noreply.github.com>
2025-10-25 13:43:14 +0700

    refactor: move handler functions to separare file
    - Move handler functions from gogator.go to handlers.go

---
+++ handlers.go
@@ -0,0 +1,104 @@
+package gogator
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/prchop/gogator/internal/database"
+)
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		log.Printf("Usage: %s <username>\n", cmd.name)
+		return fmt.Errorf("username is required")
+	}
+
+	name := cmd.args[0]
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user %q: %w", name, err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("User %q switched successfully!\n", name)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		log.Printf("Usage: %s <username>\n", cmd.name)
+		return fmt.Errorf("username is required")
+	}
+
+	name := cmd.args[0]
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Name:      name,
+		})
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("User %q created successfully:\n", name)
+	printUser(user)
+	return nil
+}
+
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetAllUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get all users: %w", err)
+	}
+	for _, u := range users {
+		if u.Name == s.cfg.UserName {
+			fmt.Printf(" * Name:    %v (current)\n", u.Name)
+			continue
+		}
+		fmt.Printf(" * Name:    %v\n", u.Name)
+	}
+	return nil
+}
+
+func printUser(u database.User) {
+	fmt.Printf(" * ID:      %v\n", u.ID)
+	fmt.Printf(" * Name:    %v\n", u.Name)
+}
+
+func handlerAggregate(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		log.Printf("Usage: %s <url>\n", cmd.name)
+		return fmt.Errorf("url is required")
+	}
+
+	feed, err := fetchFeed(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't fetch feed: %w", err)
+	}
+
+	fmt.Printf("Feed: %+v", feed)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if err := s.db.DeleteAllUsers(context.Background()); err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+	return nil
+}
