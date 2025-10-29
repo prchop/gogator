004be1a2185fea2824d9ad7be706e2f61d7b0c87
prchop <182120329+prchop@users.noreply.github.com>
2025-10-29 07:02:54 +0700

    feat: add posts table migration
    - Add schema of posts table
    - Add create and get post queries

---
+++ sql/queries/posts.sql
@@ -0,0 +1,17 @@
+-- name: CreatePost :exec
+INSERT INTO posts (
+    id,
+    created_at,
+    updated_at,
+    title,
+    url,
+    description,
+    published_at,
+    feed_id
+)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+
+-- name: GetPosts :many
+SELECT * FROM posts
+ORDER BY updated_at DESC
+LIMIT $1;
